8142c25f6b09252c00bfd597ad9caf72e6c80566
hosokawa3 <clover091039@gmail.com>
2024-07-22 05:01:07 +0000

    model作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_20_100437) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_22_045706) do
+  create_table "posts", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "article", null: false
+    t.integer "user_id", null: false
+    t.datetime "start_time", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -24,4 +34,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_20_100437) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
